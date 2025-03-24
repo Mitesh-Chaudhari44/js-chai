2396ed3c6e1508c10f464d32c8aefb0c2eb793af
Mitesh-Chaudhari44 <159791285+Mitesh-Chaudhari44@users.noreply.github.com>
2025-03-24 15:18:22 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use Strict"; // treat all code as newer version of js
+
+// alert(3 + 3);    // we are using nodejs not browser
+
+console.log(3 + 3); // code redability should be high
+console.log("Mitesh");
+
+
+let name = "Mitesh"
+let age = 19
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof "mitesh")
+console.log(typeof age);
+
+console.log(typeof null);         // object
+console.log(typeof undefined);    // undefined
+
+
