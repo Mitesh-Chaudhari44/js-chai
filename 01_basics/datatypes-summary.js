53f6cc7e8b0f057dd6ceb1ad23a418d0b779bc68
Mitesh-Chaudhari44 <159791285+Mitesh-Chaudhari44@users.noreply.github.com>
2025-03-25 19:04:32 +0000

    summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,20 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+let myYoutubeName = "hiteshchoudharydotcom";
+let anothername = myYoutubeName;
+anothername = "chaiaurcode";
+console.log(myYoutubeName);
+console.log(anothername);
+
+let userOne = {
+  email: "user@gmail.com",
+  upi: "user@ybl"
+};
+
+let userTwo = userOne;
+
+userTwo.email = "hitesh";
+console.log(userOne.email);
+console.log(userTwo.email);
