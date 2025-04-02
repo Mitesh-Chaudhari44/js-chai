0c09eeb55b17427a1e4d4cbdb03f9084ec058b16
Mitesh-Chaudhari44 <159791285+Mitesh-Chaudhari44@users.noreply.github.com>
2025-04-02 11:00:03 +0000

    objects + json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,57 +1,78 @@
 // const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = {}
 
-tinderUser.id = "123abc"
-tinderUser.name = "Mitesh"
-tinderUser.isLoggedIn = false
+// tinderUser.id = "123abc"
+// tinderUser.name = "Mitesh"
+// tinderUser.isLoggedIn = false
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-const regularUser = {
-    email: "mitesh@gmail.com",
-    fullname: {
-        userfullname: {
-            firstname: "mitesh",
-            lastname: "chaudhari"
-        }
-    }
+// const regularUser = {
+//     email: "mitesh@gmail.com",
+//     fullname: {
+//         userfullname: {
+//             firstname: "mitesh",
+//             lastname: "chaudhari"
+//         }
+//     }
+// }
+// // console.log(regularUser.fullname.userfullname.lastname);
+
+// const obj1 = { 1: "a", 2: "b" }
+
+// const obj2 = { 3: "a", 4: "b" }
+
+// // const obj4 = { 5: "a", 6: "b" }
+
+// // const obj3 = { obj1, obj2 }
+// // const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// const obj3 = { ...obj1, ...obj2 }
+
+// console.log(obj3);
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+// ]
+
+// users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course ={
+    coursename:"js chai aur code",
+    price:"999",
+    courseInstructor:"Hitesh"
 }
-// console.log(regularUser.fullname.userfullname.lastname);
 
-const obj1 = { 1: "a", 2: "b" }
+const {courseInstructor: Instructor} =course
+// console.log(courseInstructor)
+console.log(Instructor);
 
-const obj2 = { 3: "a", 4: "b" }
+// {
+//     "name":"mitesh",
+//     "course":"js in hindi",
+//     "price":"free"
+// }
 
-// const obj4 = { 5: "a", 6: "b" }
-
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
-
-const obj3 = { ...obj1, ...obj2 }
-
-console.log(obj3);
-
-const users = [
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
+[
+    {},
+    {},
+    {}
 ]
-
-users[1].email
-console.log(tinderUser);
-
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-
